c927d0dbd0fb4decc97ecc6116befc2589a584aa
Erik <elin7780@bths.edu>
2018-07-24 16:40:04 -0400

    Sentiment component will hold IBM Watson analysis of all articles

---
+++ client/src/components/Sentiment.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import data from '/Users/erik/Desktop/MVP/intelligent-investing/data/data.js';
+
+class Sentiment extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return(
+      <div></div>
+    );
+  }
+}
+
+export default Sentiment;
