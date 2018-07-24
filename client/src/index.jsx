827bfe4bc7bebca3b47fc92767288d0f4b69e9e4
Erik <elin7780@bths.edu>
2018-07-24 16:39:09 -0400

    Begin work on conditional rendering of Display component, which will contain all information

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,20 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Stock from './components/Stock.jsx';
+import Display from './components/Display.jsx';
+import Search from './components/Search.jsx';
 
 import axios from 'axios';
-import Dygraph from 'dygraphs';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      stocks: [],
+      symbol: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      symbol: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(this.state.symbol);
+    this.getStock();
+  }
+
+  getStock() {
+    var container = [];
+
+    axios.get(`https://api.iextrading.com/1.0/stock/${this.state.symbol}/chart/5y`)
+      .then(response => {
+        console.log('Response from API', response);
+
+        response.data.forEach(data => {
+          var stockInfo = [];
+
+          stockInfo.push(new Date(data.date));
+          stockInfo.push(data.close);
+
+          container.push(stockInfo);
+        });
+      })
+      .catch(err => {
+        console.error('Error fetching from API', err);
+      })
+      .then( () => {
+        this.setState({
+          stocks: container
+        });
+      });
   }
 
   render() {
     return(
-      <Stock />
-    )
+      <div>
+        <Search handleChange={this.handleChange} handleSubmit={this.handleSubmit} symbol={this.state.symbol} />
+
+        <Display stocks={this.state.stocks} />
+      </div>
+    );
   }
 };
 
