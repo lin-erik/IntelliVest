14933712291303b44345078eb0e85cdb5965e959
Erik <elin7780@bths.edu>
2018-07-25 14:45:58 -0400

    Sentiment component displays positive/neutral/negative percentage of analyzed articles

--- client/src/components/Sentiment.jsx
+++ client/src/components/Sentiment.jsx
@@ -6,11 +6,70 @@ class Sentiment extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      child_sentiment: []
+    };
+  }
+
+  extractData() {
+    data.aggregations.forEach(el => {
+      if (el.type === 'term') {
+
+        if (el.field === 'host') {
+          var container = [];
+
+          el.results.forEach(result => {
+            var query = result.aggregations[0].results;
+            var positive = 0;
+            var negative = 0;
+            var neutral = 0;
+
+            for (var i = 0; i < query.length; i++) {
+              if (query[i].key === 'positive') {
+                positive = query[i].matching_results;
+              } else if (query[i].key === 'negative') {
+                negative = query[i].matching_results;
+              } else if (query[i].key === 'neutral') {
+                neutral = query[i].matching_results;
+              }
+            }
+
+            var total = positive + negative + neutral;
+            positive = ( (positive / total) * 100).toFixed(3) + '%';
+            negative = ( (negative / total) * 100).toFixed(3) + '%';
+            neutral = ( (neutral / total) * 100).toFixed(3) + '%';
+
+            container.push({positive, negative, neutral});
+          });
+
+          this.setState({
+            child_sentiment: container
+          });
+        }
+        
+        if (el.field === 'enriched_text.sentiment.document.label') {}
+      }
+
+    });
+  }
+
+  componentDidMount() {
+    this.extractData();
   }
 
   render() {
     return(
-      <div></div>
+      <div>
+        {this.state.child_sentiment.map( (el, ind) => {
+          return(
+          <div key={ind} style={{width: '50%', height: '5%', color: '#414042', paddingTop: '1%', paddingBottom: '1%'}}>
+            <span style={{width: el.negative, height: '100%', display: 'inline-block', background:'#F7B334'}}>&nbsp;</span>
+            <span style={{width: el.neutral, height: '100%', display: 'inline-block', background: '#A7A9AC'}}>&nbsp;</span>
+            <span style={{width: el.positive, height: '100%', display: 'inline-block', background: '#D57E00'}}>&nbsp;</span>
+          </div>
+          )
+        })}
+      </div>
     );
   }
 }
