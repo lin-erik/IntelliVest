4b1a18a24cc4e7fd27aeb13b12f1b375748d1d21
Erik <elin7780@bths.edu>
2018-07-26 21:51:38 -0400

    Basic styling of Overview component

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const Overview = (props) => {
-  if (props.view === 'Top Gainers') {
+  if (props.view === 2) {
     return(
       <div className='row'>
         {props.gainers.map((stock, ind) => (
-          <div key={ind} className='col-xs-6' style={{ width: '8%', minHeight: '170px', margin: '1%', position: 'relative' }}>
-            <div>
+          <div key={ind} className='col-xs-6' style={{ width: '8%', minHeight: '170px', margin: '1%', position: 'relative', padding: '0px', backgroundColor: '#FAFAFA' }}>
+            <div style={{ padding: '10px', paddingTop: '5px' }}>
               <div style={{ fontWeight: 'bold', fontSize: '20px' }}>{stock.symbol}</div>
               <div style={{ fontSize: '10px', wordWrap: 'break-word' }}>{stock.companyName}</div>
             </div>
 
-            <div style={{ bottom: '5px', left: '50%', position: 'absolute' }}>
+            <div style={{ bottom: '5px', right: '10px', position: 'absolute', textAlign: 'right' }}>
               <div style={{ fontWeight: 'bold' }}>{stock.iexRealtimePrice}</div>
               <div style={{ fontSize: '11px' }}>{stock.changePercent}</div>
             </div>
@@ -19,17 +19,17 @@ const Overview = (props) => {
         ))}
       </div>
     );
-  } else if (props.view === 'Top Losers') {
+  } else if (props.view === 3) {
     return(
       <div className='row'>
         {props.losers.map((stock, ind) => (
-          <div key={ind} className='col-xs-6' style={{ width: '8%', minHeight: '170px', margin: '1%', position: 'relative' }}>
-            <div>
+          <div key={ind} className='col-xs-6' style={{ width: '8%', minHeight: '170px', margin: '1%', position: 'relative', padding: '0px', backgroundColor: '#FAFAFA' }}>
+            <div style={{ padding: '10px', paddingTop: '5px' }}>
               <div style={{ fontWeight: 'bold', fontSize: '20px' }}>{stock.symbol}</div>
               <div style={{ fontSize: '10px', wordWrap: 'break-word' }}>{stock.companyName}</div>
             </div>
 
-            <div style={{ bottom: '5px', left: '50%', position: 'absolute' }}>
+            <div style={{ bottom: '5px', right: '10px', position: 'absolute', textAlign: 'right' }}>
               <div style={{ fontWeight: 'bold' }}>{stock.iexRealtimePrice}</div>
               <div style={{ fontSize: '11px' }}>{stock.changePercent}</div>
             </div>
@@ -41,13 +41,13 @@ const Overview = (props) => {
     return(
       <div className='row'>
           {props.mostactive.map((stock, ind) => (
-              <div key={ind} className='col-xs-6' style={{width: '8%', minHeight: '170px', margin: '1%', position: 'relative'}}>
-                <div>
+          <div key={ind} className='col-xs-6' style={{ width: '8%', minHeight: '170px', margin: '1%', position: 'relative', padding: '0px', backgroundColor: '#FAFAFA' }}>
+                <div style={{ padding: '10px', paddingTop: '5px' }}>
                   <div style={{fontWeight: 'bold', fontSize: '20px'}}>{stock.symbol}</div>
                   <div style={{fontSize: '10px', wordWrap: 'break-word'}}>{stock.companyName}</div>
                 </div>
 
-                <div style={{bottom: '5px', left: '50%', position: 'absolute'}}>
+                <div style={{ bottom: '5px', right: '10px', position: 'absolute', textAlign: 'right' }}>
                   <div style={{fontWeight: 'bold'}}>{stock.iexRealtimePrice}</div>
                   <div style={{fontSize: '11px'}}>{stock.changePercent}</div>
                 </div>
