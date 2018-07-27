811ba06af6fa68534f878b288e3dd1a38e79725b
Erik <elin7780@bths.edu>
2018-07-26 23:04:52 -0400

    Move handling of progress bar into index.jsx

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -7,45 +7,7 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 class Display extends React.Component {
   constructor(props) {
     super(props);
-    
-    this.state = {
-      completed: 0,
-      graph_created: false,
-      analysis_complete: false
-    };
 
-    this.progress = this.progress.bind(this);
-    this.createdGraph = this.createdGraph.bind(this);
-    this.completedAnalysis = this.completedAnalysis.bind(this);
-  }
-  
-  progress() {
-    if (this.state.graph_created && this.state.analysis_complete) {
-      this.setState({ completed: 100 });
-    } else {
-      const diff = Math.random() * 2;
-      this.setState({ completed: Math.min(this.state.completed + diff, 100) });
-    }
-  };
-
-  createdGraph() {
-    this.setState({
-      graph_created: true
-    });
-  }
-
-  completedAnalysis() {
-    this.setState({
-      analysis_complete: true
-    });
-  }
-
-  componentDidMount() {
-    setInterval(this.progress, 500);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer);
   }
 
   render() {
@@ -55,12 +17,12 @@ class Display extends React.Component {
       return(
         <div style={{marginTop: '0'}}>
           <div>
-            <LinearProgress variant='determinate' value={this.state.completed} />
+            <LinearProgress variant='determinate' value={this.props.completed} />
           </div>
 
           <div style={{ paddingTop: '1%' }}>
-            <Stock key={this.props.stocks} symbol={this.props.symbol} stocks={this.props.stocks} createdGraph={this.createdGraph} />
-            <Analysis key={this.props.updateAnalysis} symbol={this.props.symbol} completedAnalysis={this.completedAnalysis} />
+            <Stock key={this.props.stocks[0]} symbol={this.props.symbol} stocks={this.props.stocks} createdGraph={this.props.createdGraph} />
+            <Analysis key={this.props.stocks[this.props.stocks.length - 1]} symbol={this.props.symbol} completedAnalysis={this.props.completedAnalysis} />
           </div>
         </div>
       );
