9488bd6ea1407130f137d455059a03d31877d137
Erik <elin7780@bths.edu>
2018-07-29 22:16:40 -0400

    Refactor to use Material UI Grid

--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
 import axios from 'axios';
--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import Grid from '@material-ui/core/Grid';
+
 const Overview = (props) => {
   if (props.view === 2) {
     return(
-      <div className='row'>
+      <Grid container spacing={8}>
         {props.gainers.map((stock, ind) => (
-          <div key={ind} className='col-xs-6' style={{ width: '8%', minHeight: '170px', margin: '1%', position: 'relative', padding: '0px', backgroundColor: '#FAFAFA' }}>
+          <Grid key={ind}
+                style={{ width: '8%', minHeight: '170px', margin: '1%', padding: '0', position: 'relative', backgroundColor: '#FAFAFA' }}
+                item xs
+                >
             <div style={{ padding: '10px', paddingTop: '5px' }}>
-              <div style={{ fontWeight: 'bold', fontSize: '20px' }}>{stock.symbol}</div>
+              <div style={{ fontWeight: 'bold', fontSize: '20px' }} onClick={props.handleClick}>{stock.symbol}</div>
               <div style={{ fontSize: '10px', wordWrap: 'break-word' }}>{stock.companyName}</div>
             </div>
 
@@ -15,17 +20,19 @@ const Overview = (props) => {
               <div style={{ fontWeight: 'bold' }}>{stock.iexRealtimePrice}</div>
               <div style={{ fontSize: '11px' }}>{stock.changePercent}</div>
             </div>
-          </div>
+          </Grid>
         ))}
-      </div>
+
+        <span style={{ fontSize: '10px', marginLeft: '1%' }}>Last update: {props.gainers[0].date}</span>
+      </Grid>
     );
   } else if (props.view === 3) {
     return(
-      <div className='row'>
+      <Grid container spacing={8}>
         {props.losers.map((stock, ind) => (
-          <div key={ind} className='col-xs-6' style={{ width: '8%', minHeight: '170px', margin: '1%', position: 'relative', padding: '0px', backgroundColor: '#FAFAFA' }}>
+          <Grid key={ind} style={{ width: '8%', minHeight: '170px', margin: '1%', padding: '0', position: 'relative', backgroundColor: '#FAFAFA' }} item xs>
             <div style={{ padding: '10px', paddingTop: '5px' }}>
-              <div style={{ fontWeight: 'bold', fontSize: '20px' }}>{stock.symbol}</div>
+              <div style={{ fontWeight: 'bold', fontSize: '20px' }} onClick={props.handleClick}>{stock.symbol}</div>
               <div style={{ fontSize: '10px', wordWrap: 'break-word' }}>{stock.companyName}</div>
             </div>
 
@@ -33,28 +40,36 @@ const Overview = (props) => {
               <div style={{ fontWeight: 'bold' }}>{stock.iexRealtimePrice}</div>
               <div style={{ fontSize: '11px' }}>{stock.changePercent}</div>
             </div>
-          </div>
+          </Grid>
         ))}
-      </div>
+
+        <span style={{ fontSize: '10px', marginLeft: '1%' }}>Last update: {props.losers[0].date}</span>
+      </Grid>
     );
   } else {
-    return(
-      <div className='row'>
-          {props.mostactive.map((stock, ind) => (
-          <div key={ind} className='col-xs-6' style={{ width: '8%', minHeight: '170px', margin: '1%', position: 'relative', padding: '0px', backgroundColor: '#FAFAFA' }}>
-                <div style={{ padding: '10px', paddingTop: '5px' }}>
-                  <div style={{fontWeight: 'bold', fontSize: '20px'}}>{stock.symbol}</div>
-                  <div style={{fontSize: '10px', wordWrap: 'break-word'}}>{stock.companyName}</div>
+    if (!props.mostactive.length) {
+      return(
+        <div style={{width: '50%', margin: 'auto', marginTop: '3%', marginBottom: '3%', textAlign: 'center'}}>Market is closed</div>
+      );
+    } else {
+      return(
+        <div className='row'>
+            {props.mostactive.map((stock, ind) => (
+            <div key={ind} className='col-xs-6' style={{ width: '8%', minHeight: '170px', margin: '1%', position: 'relative', padding: '0px', backgroundColor: '#FAFAFA' }}>
+                  <div style={{ padding: '10px', paddingTop: '5px' }}>
+                    <div style={{fontWeight: 'bold', fontSize: '20px'}} onClick={props.handleClick}>{stock.symbol}</div>
+                    <div style={{fontSize: '10px', wordWrap: 'break-word'}}>{stock.companyName}</div>
+                  </div>
+  
+                  <div style={{ bottom: '5px', right: '10px', position: 'absolute', textAlign: 'right' }}>
+                    <div style={{fontWeight: 'bold'}}>{stock.iexRealtimePrice}</div>
+                    <div style={{fontSize: '11px'}}>{stock.changePercent}</div>
+                  </div>
                 </div>
-
-                <div style={{ bottom: '5px', right: '10px', position: 'absolute', textAlign: 'right' }}>
-                  <div style={{fontWeight: 'bold'}}>{stock.iexRealtimePrice}</div>
-                  <div style={{fontSize: '11px'}}>{stock.changePercent}</div>
-                </div>
-              </div>
-          ))}
-      </div>
-    );
+            ))}
+        </div>
+      );
+    }
   }
 };
 
