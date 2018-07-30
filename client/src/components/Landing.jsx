bb4fc8a3ec43fe1ea73d1fe446ae4cfd7b6c7513
Erik <elin7780@bths.edu>
2018-07-29 22:16:17 -0400

    Extraction of date and price change for use in Overview

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -29,14 +29,20 @@ class Landing extends React.Component {
          .then(response => {
            console.log('Response from API (Overview)', response);
 
-           response.data.forEach( ({symbol, companyName, sector, iexRealtimePrice, changePercent}) => {
+           response.data.forEach( ({symbol, companyName, sector, iexRealtimePrice, latestUpdate, changePercent}) => {
+              var date = new Date(latestUpdate);
+              var month = date.getMonth() + 1;
+              var day = date.getMonth();
+              var year = date.getFullYear();
+              
+              date = month + '/' + day + '/' + year;
               changePercent = (changePercent * 100).toFixed(2) + '%';
 
               if (changePercent[0] !== '-') {
                 changePercent = '+' + changePercent;
               }
 
-             container.push({symbol, companyName, sector, iexRealtimePrice, changePercent});
+             container.push({symbol, companyName, sector, iexRealtimePrice, date, changePercent});
            });
 
            console.log(container);
@@ -68,7 +74,7 @@ class Landing extends React.Component {
       return null;
     } else {
       return(
-        <div style={{padding: '2%', width: '75%', margin: 'auto', marginTop: '3%', backgroundColor: 'white', boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'}}>
+        <div style={{padding: '2%', width: '75%', margin: 'auto', marginTop: '3%', marginBottom: '3%', backgroundColor: 'white', boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'}}>
           <h1 style={{textAlign: 'center'}}>Today's Market</h1>
           <div style={{textAlign: 'center'}}>
             <Select
@@ -83,10 +89,12 @@ class Landing extends React.Component {
           </div>
 
           <div className='container-fluid' style={{margin: '1%'}}>
-            <Overview view={this.state.view} mostactive={this.state.mostactive} gainers={this.state.gainers} losers={this.state.losers} />
+            <Overview handleClick={this.props.handleClick} view={this.state.view} mostactive={this.state.mostactive} gainers={this.state.gainers} losers={this.state.losers} />
           </div>
 
-          <div>
+          <div><hr/></div>
+
+          <div style={{margin: '2%'}}>
             <News />
           </div>
         </div>
