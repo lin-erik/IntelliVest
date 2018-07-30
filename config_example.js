af095e6766406377440089160ee71a53c0ac8360
Erik <elin7780@bths.edu>
2018-07-29 22:30:18 -0400

    Fix incorrect api key

--- config_example.js
+++ config_example.js
@@ -3,7 +3,7 @@ const DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
 const discovery = new DiscoveryV1({
   url: 'https://gateway-wdc.watsonplatform.net/discovery/api/',
   version: '2018-03-05',
-  iam_apikey: process.env.API_KEY,
+  iam_apikey: `${process.env.API_KEY}`,
 });
 
 module.exports = discovery;
